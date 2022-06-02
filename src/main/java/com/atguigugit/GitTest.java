f5888e5d0bd96c538f4260cff2a8c52de4e0564f
HanZong888 <568657687@qq.com>
2022-06-02 10:15:40 +0800

    master在第10行添加内容

--- src/main/java/com/atguigugit/GitTest.java
+++ src/main/java/com/atguigugit/GitTest.java
@@ -7,5 +7,6 @@ public class GitTest {
         System.out.println("第二次更新");
         System.out.println("第三次更新");
         System.out.println("dev添加");
+        System.out.println("master在第10行添加内容");
     }
 }
